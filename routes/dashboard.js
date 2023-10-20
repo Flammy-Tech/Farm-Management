29e7bac19059acc4e023434f376d2762a37e1165
Lewis Ng'ang'a <59686000+lewis-2000@users.noreply.github.com>
2023-10-20 10:20:42 +0300

    Added MongoDB Database
    Added MongoDB database to the dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,10 +1,44 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+const bodyParser = require('body-parser');
+
 const router = express.Router();
+
+// Initialize MongoDB Database
+const url = "mongodb://localhost:27017";
+const dbName = "FarmManagement";
+
+const client = new MongoClient(url);
+
+
+
 
 //Setup routes
 router.route('/')
     .get((req, res) =>{
         res.render('dashboard');
+
+    }).post(async(req, res) =>{
+        try {
+            const newCropType = {
+                cropName: req.body.cropName,
+                Quantity: req.body.Quantity,
+                cropPrice: req.body.cropPrice,
+                cropDescription: req.body.cropDescription
+              };
+
+            // Insert the new product into the MongoDB collection
+            const db = client.db(dbName);
+            const collection = db.collection("crops");
+            const result = await collection.insertOne(newCropType);
+
+            console.log(`Added crop with _id: ${result.insertedId}`);
+
+            res.redirect('/dashboard'); // Redirect to the product listing page
+        } catch (error) {
+            console.error('Error:', error);
+            res.status(500).send('Internal Server Error'); // Handle errors gracefully
+        }
 
     });
 
